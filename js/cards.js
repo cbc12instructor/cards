d3641e890428985ef932fc8b42aee871ae887a74
Lia Neville <lianeville5@gmail.com>
2021-06-10 19:42:44 -0400

    finished new card

--- js/cards.js
+++ js/cards.js
@@ -1,4 +1,4 @@
-var numbers = [
+/* var numbers = [
     "ace",
     "two",
     "three",
@@ -13,25 +13,92 @@ var numbers = [
     "queen",
     "king",
 ];
-var suits = ["clubs", "diamond", "heart", "spades"];
+var suits = ["clubs", "diamond", "heart", "spades"]; */
+var cardsInDeck = [
+    "ace-clubs",
+    "two-clubs",
+    "three-clubs",
+    "four-clubs",
+    "five-clubs",
+    "six-clubs",
+    "seven-clubs",
+    "eight-clubs",
+    "nine-clubs",
+    "ten-clubs",
+    "jack-clubs",
+    "queen-clubs",
+    "king-clubs",
+    "ace-diamond",
+    "two-diamond",
+    "three-diamond",
+    "four-diamond",
+    "five-diamond",
+    "six-diamond",
+    "seven-diamond",
+    "eight-diamond",
+    "nine-diamond",
+    "ten-diamond",
+    "jack-diamond",
+    "queen-diamond",
+    "king-diamond",
+    "ace-heart",
+    "two-heart",
+    "three-heart",
+    "four-heart",
+    "five-heart",
+    "six-heart",
+    "seven-heart",
+    "eight-heart",
+    "nine-heart",
+    "ten-heart",
+    "jack-heart",
+    "queen-heart",
+    "king-heart",
+    "ace-spades",
+    "two-spades",
+    "three-spades",
+    "four-spades",
+    "five-spades",
+    "six-spades",
+    "seven-spades",
+    "eight-spades",
+    "nine-spades",
+    "ten-spades",
+    "jack-spades",
+    "queen-spades",
+    "king-spades",
+    "black-joker",
+    "color-joker"
+]
+var cardsOnBoard = [];
 
-function reroll() {
-    for (i = 1; i < document.getElementById("hand").childElementCount; i++) {
-        var randomNumber = Math.floor(Math.random() * numbers.length);
-        var randomSuit = Math.floor(Math.random() * suits.length);
-        var randomCard = "card " + numbers[randomNumber] + " " + suits[randomSuit];
-        document.getElementById("card" + i).className = randomCard;
+function getCard() {
+    var randomCard = cardsInDeck[Math.floor(Math.random() * cardsInDeck.length)];
+    if (randomCard != undefined) {
+        cardsOnBoard.push(randomCard);
+        cardsInDeck.splice(cardsInDeck.indexOf(randomCard), 1);
+        randomCard= "card " + randomCard;       
+        randomCard = randomCard.replace("-", " ");
+        return randomCard;
     }
 }
-reroll();
+
+
+function reDraw() {
+    for (i = 0; i = cardsOnBoard.length; i++) {
+        cardsInDeck.push(cardsOnBoard[0]);
+        cardsOnBoard.splice(0, 1);
+    }
+    for (i = 1; i < document.getElementById("hand").childElementCount; i++) {
+        document.getElementById("card" + i).className = getCard();
+    }
+}
 
 function newCard() {
+    var randomCard = getCard();
     var node = document.createElement("div")
     node.id = "card" + (document.getElementById("hand").childElementCount);
-    var randomNumber = Math.floor(Math.random() * numbers.length);
-    var randomSuit = Math.floor(Math.random() * suits.length);
-    var randomCard = "card " + numbers[randomNumber] + " " + suits[randomSuit];
-    node.setAttribute("class", randomCard);
-    //node.appendChild(textnode);
-    document.getElementById("hand").insertBefore(node, document.getElementById("new-card"));
+    node.setAttribute("class", randomCard);    
+    document.getElementById("hand").appendChild(node);
+    document.getElementById("new-card").innerText = cardsInDeck.length
 }
