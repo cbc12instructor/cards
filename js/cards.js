56ce8db5a2cc61259e774192a26b669e3b26e8b8
Eric Harrison <github@hrrsn.co>
2021-06-10 20:48:00 -0400

    Changes to cards javascript

--- js/cards.js
+++ js/cards.js
@@ -73,22 +73,37 @@ var cardsInDeck = [
 var cardsOnBoard = [];
 
 function getCard() {
-    var randomCard = cardsInDeck[Math.floor(Math.random() * cardsInDeck.length)];
+	var randIndex = Math.floor(Math.random() * cardsInDeck.length);
+    var randomCard = cardsInDeck[randIndex];
+	console.log('we have picked -' + randomCard + ' with random index:' + randIndex);
     if (randomCard != undefined) {
         cardsOnBoard.push(randomCard);
         cardsInDeck.splice(cardsInDeck.indexOf(randomCard), 1);
         randomCard= "card " + randomCard;       
         randomCard = randomCard.replace("-", " ");
+		console.log(randomCard);
         return randomCard;
-    }
+    } else {
+		console.warn('Hey, this card is invalid - ' + randIndex);
+		return false;
+	}
 }
 
 
 function reDraw() {
+
+	console.time('Original Version');
+	//cardsInDeck = cardsInDeck.concat(cardsOnBoard);
+	//cardsOnBoard = [];
+
     for (i = 0; i = cardsOnBoard.length; i++) {
         cardsInDeck.push(cardsOnBoard[0]);
         cardsOnBoard.splice(0, 1);
     }
+	
+	console.timeEnd('Original Version');
+	
+	// todo(this can operate on cards that aren't really cards)
     for (i = 1; i < document.getElementById("hand").childElementCount; i++) {
         document.getElementById("card" + i).className = getCard();
     }
@@ -96,9 +111,11 @@ function reDraw() {
 
 function newCard() {
     var randomCard = getCard();
-    var node = document.createElement("div")
-    node.id = "card" + (document.getElementById("hand").childElementCount);
-    node.setAttribute("class", randomCard);    
-    document.getElementById("hand").appendChild(node);
-    document.getElementById("new-card").innerText = cardsInDeck.length
+	if ( randomCard ) {
+		var node = document.createElement("div")
+		node.id = "card" + (document.getElementById("hand").childElementCount);
+		node.setAttribute("class", randomCard);    
+		document.getElementById("hand").appendChild(node);
+		document.getElementById("new-card").innerText = cardsInDeck.length;
+	}
 }
